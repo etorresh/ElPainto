53c21ea4dec171b658f8ae9ddef19dd54ea27b4f
etorresh <me@etorresh.com>
2022-09-03 14:40:02 -0600

    added colorMath

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import ColorMath from "./utils/color-math";
+
+const hex = "#708238";
+console.log(ColorMath.hex2rgb(hex.slice(1)));
 
 function App() {
-  return (
-    <p>
-      Edit <code>src/App.js</code> and save to relad.
-    </p>
-  );
+  return <p>Yes</p>;
 }
 
 export default App;
---
+++ src/utils/color-math.js
@@ -0,0 +1,46 @@
+var dE00 = require("./dE00");
+
+function rgb2lab(rgb) {
+  var r = rgb[0] / 255,
+    g = rgb[1] / 255,
+    b = rgb[2] / 255,
+    x,
+    y,
+    z;
+
+  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+  x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
+  y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
+  z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
+
+  return [116 * y - 16, 500 * (x - y), 200 * (y - z)];
+}
+
+function hex2rgb(hex) {
+  var bigint = parseInt(hex, 16);
+  var r = (bigint >> 16) & 255;
+  var g = (bigint >> 8) & 255;
+  var b = bigint & 255;
+
+  return [r, g, b];
+}
+
+function getDelta(lab1, lab2) {
+  var deltaE = new dE00(lab1, lab2);
+  return deltaE.getDeltaE();
+}
+
+const ColorMath = {
+  rgb2lab: rgb2lab,
+  hex2rgb: hex2rgb,
+  getDelta: getDelta,
+};
+
+export default ColorMath;
